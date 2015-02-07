847b611664ae768b13745a9f2dc2e9969e58c90c
chandrav <c@zoosk.com>
2015-02-06 17:39:37 -0800

    First commit

---
+++ app/src/main/java/ru/chand/codepathweekoneinstagramviewer/InstagramPhoto.java
@@ -0,0 +1,14 @@
+package ru.chand.codepathweekoneinstagramviewer;
+
+/**
+ * Created by chandrav on 2/6/15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String userProfileImageUrl;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likeCounts;
+    public String timestamp;
+}
